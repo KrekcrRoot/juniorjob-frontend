d8df7a51daf78be19ee33e5e7f0651a103d584d9
Maria <mashalena123@mail.ru>
2023-11-10 00:31:50 +0300

    feat(components/user|pages/register|layouts/empty): Пустой шаблон с лого. Страница выбора регистрации, выбор роли при регистрации

---
+++ components/user/RegisterWelcome.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineEmits,watch,ref } from 'vue'
+import logoBig from "@/assets/images/big-logo.svg";
+
+const emit = defineEmits(['setrole'])
+const setRole = (role) => {
+    emit('setrole', role);
+}
+</script>
+
+<template>
+  <div class="register-welcome">
+    <img :src="logoBig" alt="" />
+    <p class="register-welcome__postitle mt-5">
+        Начни зарабатывать вместе с нами!
+    </p>
+    <div class="flex items-center gap-7 mt-7">
+        <button @click.prevent="setRole('applicant')" class="btn">Я соискатель</button>
+        <button @click.prevent="setRole('employer')" class="btn">Я работодатель</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.register-welcome {
+  width: 100%;
+  height: calc(100vh - 100px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  &__postitle {
+    color: #2c1b47;
+    text-align: center;
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+}
+</style>
---
+++ layouts/empty.vue
@@ -0,0 +1,35 @@
+<script setup>
+import logo from "@/assets/images/logo.svg";
+
+</script>
+
+<template>
+    <header class="xl:container mx-auto">
+        <div class="header-inner py-7">
+            <div class="flex gap-5 items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="10" height="20" viewBox="0 0 10 20" fill="none">
+                    <path d="M8.34235 20L0.459473 10L8.34235 0L9.74157 1.775L3.2579 10L9.74157 18.225L8.34235 20Z" fill="#AAA2D8"/>
+                  </svg>
+                  <div class="logo">
+                    <img :src="logo" alt="">
+                  </div>
+            </div>
+        </div>
+    </header>
+    <div class="xl:container mx-auto">
+        <slot/>
+    </div>
+
+</template>
+
+<style lang="scss">
+@import url("~/assets/css/main.css");
+.logo {
+    height: 51px;
+    * img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+}
+</style>
