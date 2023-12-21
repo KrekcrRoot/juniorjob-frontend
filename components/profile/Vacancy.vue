f85848f11e406505f2460792438545f2a6bf30d6
Maria <mashalena123@mail.ru>
2023-12-21 20:31:19 +0300

    Вакансия доступна гостю

--- components/profile/Vacancy.vue
+++ components/profile/Vacancy.vue
@@ -155,7 +155,7 @@ onBeforeUnmount(() => {
         <p class="profile__about mt-3">
           {{ vacancy?.time }}. {{ vacancy?.description }}
         </p>
-        <template v-if="userStore.user.role">
+        <template v-if="userStore.user && userStore.user.role !== null">
           <div
             class="w-full flex flex-col mt-3 mb-3"
             v-if="userStore.user.role.current === 'applicant'"
@@ -358,7 +358,7 @@ onBeforeUnmount(() => {
           <p class="profile__about mt-3">
             {{ vacancy?.time }}. {{ vacancy?.description }}
           </p>
-          <template v-if="userStore.user.role">
+          <template v-if="userStore.user && userStore.user.role !== null">
             <div
               class="w-full flex mt-3 mb-3 gap-4"
               v-if="userStore.user.role.current === 'applicant'"
