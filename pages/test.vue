3ca578ba560ef59b583b6f02d31e4714929d23d7
Maria <mashalena123@mail.ru>
2024-03-18 18:07:04 +0300

    test

--- pages/test.vue
+++ pages/test.vue
@@ -7,34 +7,57 @@
       title="Custom Modal Title"
     >
       <p>Here goes your custom content!</p>
-      <button @click="sendMessage">Custom Action</button>
+      <button @click="customAction">Custom Action</button>
     </UiModal>
   </div>
 </template>
 
 <script>
-// import socket from "~/plugins/socket.io";
-import { io } from "socket.io-client";
-const socket = io("wss://socket.junior-job.ru");
+import { ref } from 'vue';
+import { io } from 'socket.io-client';
+
+
 export default {
-  data() {
-    return {
-      modalOpen: false,
+
+  setup() {
+    const modalOpen = ref(false);
+    const userUuid = '8d477c7a-34be-4da3-933f-2c0d8aa53dd9';
+    const socket = io('wss://socket.junior-job.ru', {
+      path: '/',
+      extraHeaders: {
+        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZTNmZmNkMzYtODA3Mi00M2JjLTg2M2EtZGY3YmJlY2RlYzUzIiwiZW1haWwiOiJ0ZXN0dGVzdEBtYWlsLnJ1Iiwicm9sZSI6ImxlZ2FsX2VudGl0eSIsImlhdCI6MTcxMDc2NjI5NiwiZXhwIjoxNzEwNzY2ODk2fQ.RQSn6QgHKDQ6fXq5T8UdBqxOOkBpAzN7Xw0HOZtNYhs'
+      },
+      auth: {
+        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZTNmZmNkMzYtODA3Mi00M2JjLTg2M2EtZGY3YmJlY2RlYzUzIiwiZW1haWwiOiJ0ZXN0dGVzdEBtYWlsLnJ1Iiwicm9sZSI6ImxlZ2FsX2VudGl0eSIsImlhdCI6MTcxMDc2NjI5NiwiZXhwIjoxNzEwNzY2ODk2fQ.RQSn6QgHKDQ6fXq5T8UdBqxOOkBpAzN7Xw0HOZtNYhs'
+      },
+      query: { user_uuid: userUuid },
+      transports: ['websocket']
+    });
+
+    socket.on('incoming', (data) => {
+      console.log('Incoming data:', data);
+    });
+
+    socket.on('debug', (data) => {
+      console.log('Debug:', data);
+    });
+
+    const openModal = () => {
+      modalOpen.value = true;
     };
-  },
-  methods: {
-    openModal() {
-      this.modalOpen = true;
-    },
-    customAction() {
-      console.log("Performing custom action...");
-    },
-    sendMessage() {
-      let message = {
-        body: "text from app",
+
+    const customAction = () => {
+      console.log('Performing custom action...');
+    };
+
+    const sendMessage = () => {
+      const message = {
+        body: 'text from app'
       };
-      socket.emit("message", message);
-    },
-  },
+      socket.emit('message', message);
+    };
+
+    return { modalOpen, openModal, customAction, sendMessage };
+  }
 };
 </script>
